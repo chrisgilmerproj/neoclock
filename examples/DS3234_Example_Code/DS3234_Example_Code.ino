f1d9b48e45cf1557a4115f1309484fac7be61e87
Chris Gilmer <chris.gilmer@gmail.com>
2014-06-28 14:41:08 -0700

    Break out logic into different methods

--- examples/DS3234_Example_Code/DS3234_Example_Code.ino
+++ examples/DS3234_Example_Code/DS3234_Example_Code.ino
@@ -27,37 +27,96 @@ int RTC_init(){
       delay(10);
 }
 //=====================================
-int SetTimeDate(int d, int mo, int y, int h, int mi, int s){
-    // Seconds, Minutes, Hour, Weekday, Day, Month, Year
-    int TimeDate [7]={s,mi,h,0,d,mo,y};
+// Methods to set time
+// 0x80 - Second
+// 0x81 - Minute
+// 0x82 - Hour
+// 0x83 - Weekday
+// 0x84 - Day
+// 0x85 - Month
+// 0x86 - Year
+//=====================================
 
-    // Walk through each item in the TimeDate array
-    for(int i=0; i<=6;i++){
-        // Skip Weekday
-        if(i==3) {
-            i++;
-        }
+void setValue(int address, int val){
+    // Convert the values to hex
+    int b = val/10;   // 10's spot
+    int a = val-b*10; //  1's spot
 
-        // Convert the values to hex
-        int b = TimeDate[i]/10;   // 10's spot
-        int a = TimeDate[i]-b*10; //  1's spot
-
-        // For hour
-        if(i==2){
-            if (b==2)
-                b=B00000010;
-            else if (b==1)
-                b=B00000001;
-        }
-        // Write the hex value back
-        TimeDate[i]= a+(b<<4);
-
-        digitalWrite(cs, LOW);
-        SPI.transfer(i+0x80);
-        SPI.transfer(TimeDate[i]);
-        digitalWrite(cs, HIGH);
+    // For hour
+    if (address == 0x82) {
+      if (b==2) {
+          b=B00000010;
+      }
+      else if (b==1) {
+          b=B00000001;
+      }
     }
+
+    // Write the hex value back
+    int out_val = a+(b<<4);
+
+    digitalWrite(cs, LOW);
+    SPI.transfer(address);
+    SPI.transfer(out_val);
+    digitalWrite(cs, HIGH);
 }
+
+void setSecond(int val){
+    int address = 0x80;
+    setValue(address, val);
+}
+
+void setMinute(int val){
+    int address = 0x81;
+    setValue(address, val);
+}
+
+void setWeekday(int val){
+    int address = 0x82;
+    setValue(address, val);
+}
+
+void setHour(int val){
+    int address = 0x82;
+    setValue(address, val);
+}
+
+void setDay(int val){
+    int address = 0x84;
+    setValue(address, val);
+}
+
+void setMonth(int val){
+    int address = 0x85;
+    setValue(address, val);
+}
+
+void setYear(int val){
+    int address = 0x86;
+    setValue(address, val);
+}
+
+void SetTime(int h, int mi, int s){
+    setHour(h);
+    setMinute(mi);
+    setSecond(s);
+}
+
+void SetDate(int d, int mo, int y){
+    setDay(d);
+    setMonth(mo);
+    setYear(y);
+}
+
+void SetTimeDate(int d, int mo, int y, int h, int mi, int s){
+    setDay(d);
+    setMonth(mo);
+    setYear(y);
+    setHour(h);
+    setMinute(mi);
+    setSecond(s);
+}
+
 //=====================================
 String ReadTimeDate(){
     String temp;
